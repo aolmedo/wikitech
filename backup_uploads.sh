e550634bb6c806819570571693b06fb9c3081b4e
Agustín <aolmedo87@gmail.com>
2015-03-01 19:55:23 -0300

    Scripts para hacer los backups de la base de datos y para hacer los backups de los archivos subidos a traves del sistema.

--- backup_uploads.sh
+++ backup_uploads.sh
@@ -1,5 +1,38 @@
-date=`date +"%Y%m%d_%H%M%N"`
-filename="/home/ubuntu/backups_db/uploads_${date}.zip"
+#!/bin/sh
+
 uploads_directory="/home/ubuntu/loyalty/lmg-formosa-fidelitas-2/media/uploads/"
+
+# Ayuda y modo de empleo
+mostrarAyuda () {
+echo -e "$0 | USO:"
+echo -e "$0 | \t./backups_uploads.sh [-i <PATH> | -f <PATH> | -h]"
+echo -e "$0 | OPCIONES:"
+echo -e "$0 | \t-i\tPath completo del archivo origen."
+echo -e "$0 | \t-f\tPath completo del archivo destino."
+echo -e "$0 | \t-h\tMuestra esta ayuda"
+}
+
+# Procesamiento de parámetros
+while getopts 'i:f:h' OPCION; do
+  case $OPCION in
+    i)
+      uploads_directory="$OPTARG"
+      ;;
+    f)
+      path="$OPTARG"
+      ;;
+    h)
+      mostrarAyuda
+      ;;
+    ?)
+      echo -e "$0 | ERROR: -$OPTARG no es un parámetro válido."
+      mostrarAyuda
+      exit 1;
+      ;;
+  esac
+done
+
+date=`date +"%Y%m%d_%H%M%N"`
+filename="${path}/uploads_${date}.zip"
 
 zip -r $filename $uploads_directory 
--- dump_db.sh
+++ dump_db.sh
@@ -1,6 +1,37 @@
 #!/bin/sh
-path="/home/ubuntu/backups_db"
+path="/home/ubuntu/backups"
 dbname="formosa"
+
+# Ayuda y modo de empleo
+mostrarAyuda () {
+echo -e "$0 | USO:"
+echo -e "$0 | \t./dump_db.sh [-d <DBNAME> | -f <PATH> | -h]"
+echo -e "$0 | OPCIONES:"
+echo -e "$0 | \t-d\tNombre de la base de datos."
+echo -e "$0 | \t-f\tPath completo del archivo destino."
+echo -e "$0 | \t-h\tMuestra esta ayuda"
+}
+
+# Procesamiento de parámetros
+while getopts 'd:f:h' OPCION; do
+  case $OPCION in
+    d)
+      dbname="$OPTARG"
+      ;;
+    f)
+      path="$OPTARG"
+      ;;
+    h)
+      mostrarAyuda
+      ;;
+    ?)
+      echo -e "$0 | ERROR: -$OPTARG no es un parámetro válido."
+      mostrarAyuda
+      exit 1;
+      ;;
+  esac
+done
+
 date=`date +"%Y%m%d_%H%M%N"`
 filename="${path}/${dbname}_${date}.gz"
 pg_dump $dbname | gzip > $filename
