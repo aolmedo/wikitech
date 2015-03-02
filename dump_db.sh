ae69c7a3f1c7931390fcbfb0e0dd9e25d2bcbf2d
Agustín <aolmedo87@gmail.com>
2015-03-02 13:53:22 -0300

    Corrección del script para que tome el usuario y la contraseña.

--- dump_db.sh
+++ dump_db.sh
@@ -8,15 +8,23 @@ echo -e "$0 | USO:"
 echo -e "$0 | \t./dump_db.sh [-d <DBNAME> | -f <PATH> | -h]"
 echo -e "$0 | OPCIONES:"
 echo -e "$0 | \t-d\tNombre de la base de datos."
+echo -e "$0 | \t-u\tUsuario de la base de datos"
+echo -e "$0 | \t-p\tPassword del usuario de la base de datos"
 echo -e "$0 | \t-f\tPath completo del archivo destino."
 echo -e "$0 | \t-h\tMuestra esta ayuda"
 }
 
 # Procesamiento de parámetros
-while getopts 'd:f:h' OPCION; do
+while getopts 'd:u:p:f:h' OPCION; do
   case $OPCION in
     d)
       dbname="$OPTARG"
+      ;;
+    u)
+      user="$OPTARG"
+      ;;
+    p)
+      password="$OPTARG"
       ;;
     f)
       path="$OPTARG"
@@ -34,4 +42,4 @@ done
 
 date=`date +"%Y%m%d_%H%M%N"`
 filename="${path}/${dbname}_${date}.gz"
-pg_dump $dbname | gzip > $filename
+pg_dump -U $user -W $password $dbname | gzip > $filename
